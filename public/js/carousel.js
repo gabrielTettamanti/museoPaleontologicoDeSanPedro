026fce41b2d1a3347f755cd36634fddc9038975d
Ignacio Lopez <ignaciolopestela12@gmail.com>
2022-06-25 11:51:12 -0300

    Creo el carrusel

---
+++ public/js/carousel.js
@@ -0,0 +1,34 @@
+const slides = document.querySelectorAll('.slide');
+const $forwardButton = document.querySelector('.forward');
+const $backButton = document.querySelector('.back');
+
+let currentSlide = 0;
+let maxSlides = slides.length - 1;
+
+slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${index * 100}%)`;
+});
+
+$forwardButton.addEventListener('click', () => {
+    if(currentSlide === maxSlides){
+        currentSlide = 0;
+    }else{
+        currentSlide ++;
+    }
+
+    slides.forEach((slide, index) => {
+        slide.style.transform = `translateX(${100 * (index - currentSlide) }%)`;
+    });
+});
+
+$backButton.addEventListener('click', () => {
+    if(currentSlide === 0){
+        currentSlide = maxSlides;
+    }else{
+        currentSlide --;
+    }
+
+    slides.forEach((slide, index) => {
+        slide.style.transform = `translateX(${100 * (index -currentSlide) }%)`;
+    });
+});

c75b8b439fb993f6eac14d00b0e258c4b0ee84ac
AlejandraDW4 <sofiagutierrez1845@gmail.com>
2022-06-28 13:31:32 -0300

    Cambie la velocidad de movimiento del fondo

--- public/js/movingBackground.js
+++ public/js/movingBackground.js
@@ -1,8 +1,8 @@
 window.onload = function() {
     document.onmousemove = function(e) {
-        const x = -(e.clientX/10);
+        const x = -(e.clientX/25);
 
-        const y = -(e.clientY/10);
+        const y = -(e.clientY/25);
 
       this.body.style.backgroundPosition = x + 'px ' + y + 'px';
     };

788fd98d94035abe101658bdac74b5079c4742f2
Ignacio Lopez <ignaciolopestela12@gmail.com>
2022-06-29 12:57:09 -0300

    Arreglo modals de noticias

--- public/js/fullPage.js
+++ public/js/fullPage.js
@@ -1,5 +1,6 @@
 new fullpage('#fullPage', {
     autoScrolling: true,
     navigation: true,
-    navigationTooltips: ['Home', 'Novedades', 'Sponsors', 'Noticias', 'Nuestra historia', 'Contacto']
+    navigationTooltips: ['Home', 'Novedades', 'Sponsors', 'Noticias', 'Nuestra historia', 'Contacto'],
+    css3:false
 })

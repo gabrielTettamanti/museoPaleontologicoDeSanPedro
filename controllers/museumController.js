a17b08497432ae16b397226d2bb035eae41ef467
AlejandraDW4 <sofiagutierrez1845@gmail.com>
2022-07-02 17:47:52 -0300

    creando ejs y sass de countryside

--- controllers/museumController.js
+++ controllers/museumController.js
@@ -5,8 +5,11 @@ const newsFilePath = path.join(__dirname, '../data/news.json');
 const news = JSON.parse(fs.readFileSync(newsFilePath, 'utf-8'));
 
 const museumController = {
-    list: (req, res) => {
+    tour: (req, res) => {
         res.render("museumTour", {news});
+    },
+    visit: (req, res) => {
+        res.render("countryside");
     }
 };
 

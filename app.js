b52b991050e671a8d06b0706d9c7cd9d859a141f
MarinaDiez <marinafdiez@gmail.com>
2022-07-21 15:44:11 -0300

    cree pagina 404, la ruta y la logica

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const publicPath = path.resolve(__dirname, 'public');
 app.use( express.static(publicPath));
 app.use(morgan('dev'));
 
+
 //***** Template engine *****/
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -26,6 +27,7 @@ const indexRouter = require("./routers/indexRouter");
 const newsRouter = require("./routers/newsRouter");
 const museumRouter = require("./routers/museumRouter");
 const adminRouter = require("./routers/adminRouter");
+const errorRouter = require("./routers/errorRouter");
 
 //***** Index Router  *****/
 app.use("/", indexRouter);
@@ -38,3 +40,21 @@ app.use("/museum", museumRouter);
 
 //***** Admin router  *****/
 app.use("/admin", adminRouter);
+
+//***** error 404 router  *****/
+app.use("/404", errorRouter);
+
+// ** catch 404 and forward to error handler **
+app.use((req, res, next) => next(createError(404)));
+
+// ** error handler **
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.path = req.path;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
